434cdc5c392ccc0b86195273bbecd1a3a865fe70
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-06-24 10:30:46 +0000

    Pseudo code for game logic added

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* Logic for the game: all possible ways to win on an array with 9 values: 
+n + 2 => n is a factor of 105 (3, 5, 7) && !1 - covers the right side slash /
+n + 3 => all downwards squares 
+n + 4 => (9-4-4), left to left side slash \
+n + 1 =>  no good logic yet, (n % 3 === '0' && arr[i] === "o" || n % 3 === '0' && arr[i] === "x") - doesn't work reliably
+
 const gameboard = {
     gameboard: [],
 }
