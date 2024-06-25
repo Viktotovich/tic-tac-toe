cb29ee2fadaf5e0faef92db91d2228ef6a85195b
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-06-25 07:01:23 +0400

    "Correct logic for n+1, added Modal HTML, IIFE added for private variables"

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-/* Logic for the game: all possible ways to win on an array with 9 values: 
+/* Logic for the game: all possible ways to win on an array with 9 values:
 n + 2 => n is a factor of 105 (3, 5, 7) && !1 - covers the right side slash /
-n + 3 => all downwards squares 
-n + 4 => (9-4-4), left to left side slash \
-n + 1 =>  no good logic yet, (n % 3 === '0' && arr[i] === "o" || n % 3 === '0' && arr[i] === "x") - doesn't work reliably
+n + 3 => all downwards squares
+n + 4 => (squares 9, 5, 1 (9-4)), right to left side slash \
+n + 1 =>  split array in 3 parts, 123, 456, 789, and look if all 3 match.
+*/
 
 const gameboard = {
     gameboard: [],
+    gameflow: (function(){
+
+        function Player(player, fishka, score){
+            this.player = player;
+            this.fishka = fishka;
+            this.score = score;
+        }
+
+        function getName(name) {
+            player1Name = document.querySelector("#player1-name")
+            player2Name = document.querySelector("#player2-name")
+        }
+
+    })(),
 }
