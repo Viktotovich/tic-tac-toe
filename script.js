eccc53fe4a8b170b6f0ba137a083dcf82a9f895b
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-06-28 08:00:44 +0400

    "Complete refactor of code while maintaining the same functionalities"

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+const gameboard = {
+    gameboard: [],
+    rounds: [],
+    fishkas: [],
+    players: [],
+
+    outsourceLogic: function(){
+        gameRules.n1();
+        gameRules.n2();
+        gameRules.n3();
+        gameRules.n4();
+    },
+    markSquare: function(e){
+        const targetElement = e.target;
+        //Figure this out
+    }
+};
+
 /* Logic for the game: all possible ways to win on an array with 9 values:
 n + 2 => (3, 5, 7) - covers the right side slash /
 n + 3 => all downwards squares (i loop + 3)
 n + 4 => (squares 9, 5, 1 (9-4)), right to left side slash (i loop + 4) \
 n + 1 =>  split array in 3 parts, 123, 456, 789, and look if all 3 match. */
-
-// modular javascript - will fix when I get home and have time
-const gameboard = {
-    gameboard: [],
-    
-    //logic checks - easier to fix when something doesnt work
+const gameRules = {
     n1: function(){
-        var firstColumn = this.gameboard.slice(0, 2);
-        var secondColumn = this.gameboard.slice(3, 5);
-        var thirdColumn = this.gameboard.slice(6, 8);
+        var firstColumn = gameboard.gameboard.slice(0, 2);
+        var secondColumn = gameboard.gameboard.slice(3, 5);
+        var thirdColumn = gameboard.gameboard.slice(6, 8);
 
         this.victoryCondition(firstColumn);
         this.victoryCondition(secondColumn);
@@ -22,8 +35,8 @@ const gameboard = {
         var column = [];
         let boardPart;
 
-        for (let i = 2; i < 9; i + 2 ) {
-            boardPart = this.gameboard.slice(i);
+        for (let i = 2; i < 9; i += 2 ) {
+            boardPart = gameboard.gameboard.slice(i);
             column.push(boardPart);
         }
 
@@ -34,7 +47,7 @@ const gameboard = {
         let boardPart;
 
         for (let i = 0; i < 9; i + 3) {
-            boardPart = this.gameboard.slice(i);
+            boardPart = gameboard.gameboard.slice(i);
             column.push(boardPart);
         }
 
@@ -45,20 +58,19 @@ const gameboard = {
         let boardPart;
 
         for (let i = 0; i < 9; i + 4) {
-            boardPart = this.gameboard.slice(i);
+            boardPart = gameboard.gameboard.slice(i);
             column.push(boardPart);
         }
-
         this.victoryCondition(column)
     },
     victoryCondition: function(column){
-    reducedColumn = column.reduce((accumulator, currentValue) => accumulator + currentValue);
-    return reducedArray;
-    //works, production ready
-    }
+        reducedColumn = column.reduce((accumulator, currentValue) => accumulator + currentValue);
+        return reducedColumn;
+        //works, production ready
+        },
 };
 
-
+//refactored IIFE as it was trying to do too much
 (function(){
     function renderListeners(){
         document.addEventListener("DOMContentLoaded", openModal);
@@ -68,8 +80,6 @@ const gameboard = {
         submit.addEventListener("click", createBoard);
     }
 
-    renderListeners();
-
     function createBoard() {
         let square;
         gameboard.gameboard.splice(0,9,null,null,null,null,null,null,null,null,null);
@@ -77,62 +87,37 @@ const gameboard = {
         square = document.createElement("div")
         square.setAttribute("id", `${i}`);
         square.setAttribute("class", "game-square");
-        square.addEventListener("click", markSquare);
+        square.addEventListener("click", gameboard.markSquare);
 
         const keyDOM = document.querySelector(".tic-tac-toe-container");
         keyDOM.appendChild(square);
         }
     }
 
-    function markSquare(e){
-        const targetElement = e.target;
-        //issue: need to know what player and fishka
-    }
-
-    function Player(player, fishka, score){
-        this.player = player;
-        this.fishka = fishka;
-        this.score = score;
-    }
-
-    Player.prototype.increaseScore = function() {
-        this.score += 1;
-    }
-
-    Player.prototype.resetScore = function() {
-        this.score *= 0;
-    }
-
-    function getPlayerValues() {
-        player1Name = document.querySelector("#player1-name").textContent;
-        player2Name = document.querySelector("#player2-name").textContent;
-        player1Fishka = document.querySelector("#player1-fishka").value;
+    function getPlayerValues(){
+        const player1Name = document.querySelector("#player1-name").value;
+        const player2Name = document.querySelector("#player2-name").value;
+        const player1Fishka = document.querySelector("#player1-fishka").value;
         let player2Fishka;
 
         if (player1Name === '' || player2Name === '') {
             var warning = document.querySelector("#warning-text");
             warning.textContent = "Please fill in all the necessary details before proceeding";
         } else {
+            var warning = document.querySelector("#warning-text");
             warning.textContent = '';
             closeModal();
-            
-        if (player1Fishka === "O") {
-            player2Fishka = "X";
-        } else {
-            player2Fishka = "O";
-        }
-      
-        const player1 = new Player(player1Name, player1Fishka, 0);
-        const player2 = new Player(player2Name, player2Fishka, 0);
-
-            return {
-                player1,
-                player2
-                }
-            //Anything run after getPlayerValues is A-Okay to access player1 and 2's scores and methods
+            if (player1Fishka === "O") {
+                player2Fishka = "X";
+            } else {
+                player2Fishka = "O";
             }
-    }
-    
+            const player1 = createPlayer(player1Name, player1Fishka);
+            const player2 = createPlayer(player2Name, player2Fishka);
+            gameboard.players.push(player1, player2);
+        }
+    };
+
     function closeModal(){
         const modal = document.querySelector("dialog")
         modal.close();
@@ -143,16 +128,21 @@ const gameboard = {
         modal.showModal();
     }
 
-    function handleLogic(){
-        gameboard.n1();
-        gameboard.n2();
-        gameboard.n3();
-        gameboard.n4();
-    }
+    renderListeners();
+})();
 
-    return {
-        handleLogic,
-        player1,
-        player2,
+function createPlayer(name, fishka){
+    const playerName = name;
+    var score = 0;
+
+    const awardScore = () => score++;
+    const getScore = () => score;
+    const getFishka = () => fishka;
+    const getPlayerName = () => playerName;
+
+    return{
+        awardScore,
+        getScore,
+        getFishka
     }
-})()
+}
